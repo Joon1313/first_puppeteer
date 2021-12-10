5b4b439d1bf1c3df9d4e4b6ea915452bd240d715
Joon1313 <wkdrud13@gmail.com>
2021-12-10 14:39:00 +0900

    cam-loa comment crawling

--- main.ts
+++ main.ts
@@ -1,12 +1,30 @@
-// 설치된 puppeteer 모듈
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  // const browser = await puppeteer.launch({devtools: true});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://cam-loa.com/comment");
-  await page.waitForTimeout(5000);
-  await page.screenshot({ path: "./img/cam-loa-comment.png" });
+  await page.waitForTimeout(2000);
+  const camloaComment = await page.evaluate(()=>{
+    let list = [];
+    const commentList = document.querySelector('.MuiList-root').children;
+    for (let i = 0; i < commentList.length; i += 2) {
+      const comment = commentList[i].children[0].children[1].children[1].innerHTML;
+      list.push(comment);
+    }
+    return list;
+  })
+  // console.log(camloaComment)
 
-  await browser.close();
+  fs.writeFile("./json/cam-loa-comment.json",
+  JSON.stringify(camloaComment, null, 2),
+  err =>
+    err ? console.error("error", err)
+       : console.log("Success")
+);
+  // await page.screenshot({ path: "./img/cam-loa-comment.png" });
+
+  // await browser.close();
 })();
